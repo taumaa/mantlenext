48ed3247345c5c18f72ee0405cfc71c64572a248
tauma <contact@part.gg>
2023-01-15 01:52:37 +0100

    WIP

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div>
+            <Head>
+                <title>Mantle Punks</title>
+
+            </Head>
+
+            <Component {...pageProps} />
+      </div>
+  )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+
+    render() {
+        return (
+            <Html className="dark">
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="/icon.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/icon.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/icon.png"/>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
+export default MyDocument
