1be4c487d8aedbc6c40ce21e9f5993774b28d57e
tauma <contact@part.gg>
2023-01-19 17:21:34 +0100

    wip

--- src/sections/Mint.jsx
+++ src/sections/Mint.jsx
@@ -4,17 +4,11 @@ import useMint from "@/hooks/useMint";
 import MintModal from "@/components/Modal/MintModal";
 import {useEffect} from "react";
 import {useState} from "react";
-import {useAccount} from "wagmi";
-import {ownerAddress} from "@/const";
 
 const Mint = () => {
 
     const [modalOpen , setModalOpen] = useState(false)
     const [currentTxHash, setCurrentTxHash] = useState(null)
-
-
-
-    const {address} = useAccount()
 
     const {humanReadableSupply, isLoading: isLoadingSupply} = useMinted();
     const {write, isSuccess} = useMint({
@@ -29,7 +23,7 @@ const Mint = () => {
     }, [modalOpen])
 
     return (
-        <div className="justify-between flex lg:flex-row flex-col py-32">
+        <div className="justify-between flex lg:flex-row flex-col py-32 lg:space-y-0 space-y-16">
             <div className="flex items-center">
                 <div className="-mb-8">
                     <div className="mb-8 text-white text-xs">
@@ -47,12 +41,12 @@ const Mint = () => {
                     </div>
                     { !!humanReadableSupply && !isLoadingSupply && (
                         <div className="mt-6 text-white text-sm">
-                            Minted: {humanReadableSupply}/<span className="text-3xl">&infin;</span>
+                            Minted: {humanReadableSupply}/<span className="text-xl">&infin;</span>
                         </div>
                     )}
                     {!humanReadableSupply && (
                         <div className="mt-3 text-white text-sm">
-                            Minted: .../<span className="text-3xl">&infin;</span>
+                            Minted: .../<span className="text-xl">&infin;</span>
                         </div>
                     )}
                     <span className="text-xs text-white">(Max supply disabled for testnet)</span>
